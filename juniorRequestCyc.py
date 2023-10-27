ec570392f752c5046b7f91d60ccf5652c84569e7
kxv <136233650+kxvvv@users.noreply.github.com>
2023-10-27 13:38:28 +0300

    Update juniorRequestCyc.py

--- juniorRequestCyc.py
+++ juniorRequestCyc.py
@@ -1,4 +1,6 @@
 import discord
+from datetime import datetime as dt
+from pytz import timezone
 
 async def juniorRequestFunc(client):
     request = client.get_channel(997923048403509308)
@@ -11,18 +13,36 @@ async def juniorRequestFunc(client):
     requestDict = {}
     text = ""
 
+    def whenCreated(createdDate):
+        if dt.now().day == createdDate.day:
+            return f'в {createdDate.hour}:{createdDate.minute}.'
+
+        result = dt.now().day - createdDate.day
+
+        if result == 1:
+            dayWord = 'день'
+        elif result <= 4:
+            dayWord = 'дня'
+        else:
+            dayWord = 'дней'
+
+        
+
+        return f'{result} {dayWord} назад, в {createdDate.hour}:{createdDate.minute}.'
+         
+
     for x in m:
         if x.embeds != []:
 
             if "ожидает одобрения" in x.embeds[0].title:
                 requestCounter+=1
-                requestDict[f"{requestCounter}"] = f"{x.id}"
+                requestDict[f"{requestCounter}"] = {'id': f'{x.id}', 'date': f'{whenCreated(x.created_at.astimezone(timezone("Europe/Moscow")))}'}
 
     titleText = f"Господа модераторы и модераторанесы, количество не одобренных запросов в <#997923048403509308> - {requestCounter}\n"
 
 
     for x in range(1, len(requestDict)+1):
-        text += f"Запрос номер `{x}` - <#" + requestDict[f"{x}"] + ">\n"
+        text += f'Запрос номер `{x}` - <#{requestDict[f"{x}"]["id"]}>, создан {requestDict[f"{x}"]["date"]}\n'
 
     if text == "":
         await ctx.send("Не обработанных запросов в <#997923048403509308> нет, хорошая работа, господа модераторы и модераторанесы.")
